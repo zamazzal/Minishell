089bc4ae09cf9a381bd3b308943905e559a139b4
Zouhair Amazzal <zamazzal@e2r13p3.1337.ma>
2019-04-19 15:28:57 +0100

    done

--- run.sh
+++
@@ -1,4 +0,0 @@
-make -C libft re
-make -C libft clear
-gcc -Wall -Wextra -Werror srcs/builtins/*.c srcs/commands.c srcs/main.c srcs/env.c srcs/tools.c srcs/messages.c -I includes -I libft/ libft/libft.a -I srcs/builtins/ls/includes srcs/builtins/ls/srcs/*
-echo "done"
